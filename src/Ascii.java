35a63a003105782dafb183f27899e9773488a3ea
Matthew <matthew.d.baker20@gmail.com>
2020-12-18 17:54:35 -0600

    created class for ascii art

---
+++ src/Ascii.java
@@ -0,0 +1,16 @@
+public class Ascii {
+
+    public static void art1(){
+        System.out.println("       ^      ");
+        System.out.println("     / •  \\    ");
+        System.out.println("    /      \\    ");
+        System.out.println("   /  •  •  \\    ");
+        System.out.println("  /      •   \\    ");
+        System.out.println(" /     •      \\    ");
+    }
+
+    public static void main(String[] args) {
+        art1();
+    }
+
+}
