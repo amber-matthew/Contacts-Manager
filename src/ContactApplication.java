413f79d0ddf583f2ca736aff4ffca404914cdd2a
Matthew <matthew.d.baker20@gmail.com>
2021-01-04 14:43:27 -0600

    working on case insensitive searching of existing contacts

--- src/ContactApplication.java
+++ src/ContactApplication.java
@@ -22,7 +22,7 @@ public class ContactApplication {
 
         art.art2();
         do{
-            int choice = displayHomeScreen(sc, contactsList);
+            int choice = displayHomeScreen(sc, contactsList, allContacts);
             System.out.println(" ");
             if(choice == 5){
                 notExit = false;
@@ -39,17 +39,17 @@ public class ContactApplication {
     }
 
 
-    public static int displayHomeScreen(Input sc, HashMap<String, String> allContacts) throws IOException {
+    public static int displayHomeScreen(Input sc, HashMap<String, String> allContacts, List<String> contactList) throws IOException {
         System.out.println("1 - View contacts");
         System.out.println("2 - Add a new contact");
         System.out.println("3 - Search a contact by name");
         System.out.println("4 - Delete an existing contact");
         System.out.println("5 - Exit");
 
-        return runUserOption(sc.getNumber("Enter an option (1, 2, 3, 4, or 5)", 1,5), sc, allContacts);
+        return runUserOption(sc.getNumber("Enter an option (1, 2, 3, 4, or 5)", 1,5), sc, allContacts, contactList);
     }
 
-    public static int runUserOption(int option, Input sc, HashMap<String, String> allContacts) throws IOException {
+    public static int runUserOption(int option, Input sc, HashMap<String, String> allContacts, List<String> contactList) throws IOException {
         switch(option){
             case 1:
                 viewAllContacts(allContacts);
@@ -61,7 +61,7 @@ public class ContactApplication {
                 searchByName(sc, allContacts);
                 break;
             case 4:
-                deleteContact(sc, allContacts);
+                deleteContact(sc, allContacts, contactList);
                 break;
         }
         return option;
@@ -70,13 +70,16 @@ public class ContactApplication {
     public static void addNewContact(Input sc, HashMap<String, String> allContacts) throws IOException {
         String inputName = sc.getString("What is this person's name?");
         String[] names = inputName.split(" ");
-        String firstL = names[0].substring(0, 1).toUpperCase();
-        String restOfFirstName = names[0].substring(1);
-
         String lastL = names[1].substring(0, 1).toUpperCase();
-        String restOfLastName = names[1].substring(1);
+        String restOfLastName = names[1].substring(1).toLowerCase();
+        String firstL = names[0].substring(0, 1).toUpperCase();
+        String restOfFirstName = names[0].substring(1).toLowerCase();
 
         String newContactName = firstL+restOfFirstName+" "+lastL+restOfLastName;
+
+
+
+
         FileReader contactReader = new FileReader("src", "contacts.txt", "contacts.txt");
         FileReader logWriter = new FileReader("src", "contacts.log", "contacts.log");
 
@@ -215,7 +218,7 @@ public class ContactApplication {
 
 
 
-    public static  void deleteContact(Input sc, HashMap<String, String> allContacts) throws IOException {
+    public static  void deleteContact(Input sc, HashMap<String, String> allContacts, List<String> contactList) throws IOException {
         boolean validChoice = true;
         System.out.println("Current contact list: ");
         viewAllContacts(allContacts);
@@ -223,7 +226,8 @@ public class ContactApplication {
         String choice = sc.getString("What contact do you want to delete?");
 
         try{
-            allContacts.get(choice);
+            int index = contactList.indexOf(choice);
+            contactList.get(index);
 
         } catch (Exception e){
             FileReader logWriter = new FileReader("src", "contacts.log", "contacts.log");
